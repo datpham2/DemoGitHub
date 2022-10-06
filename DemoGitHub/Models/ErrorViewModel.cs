74fb75594e5f63d71173b6a060339bae76b48797
datpham2 <0306181218@caothang.edu.vn>
2022-10-06 14:52:15 +0700

    Add project files.

---
+++ DemoGitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoGitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
